55f92090bdf1da64f930e464456e5d0e354b7ef7
Abey <abey.yoseph@leidos.com>
2022-11-15 12:30:21 -0500

    modified spat plotting check in

--- tsmo/src/modified_spat_parser.py
+++ tsmo/src/modified_spat_parser.py
@@ -24,7 +24,7 @@ def kafkaParser():
             #write data of interest to csv which will be used to produce plots
             with open(f'{output_directory_path}/{fileName}_parsed.csv', 'w', newline='') as write_obj:
                 csv_writer = writer(write_obj)
-                csv_writer.writerow(["Intersection_Name", "Intersection_ID", "Moy", "Timestamp", "Signal_Group", "Event_State"])
+                csv_writer.writerow(["Intersection_Name", "Intersection_ID", "Moy", "Timestamp", "Signal_Group", "Event_State", "Event_State_Color"])
 
                 #extract relevant elements from the json
                 for i in range(0, len(textList)):
@@ -57,7 +57,7 @@ def kafkaParser():
                                 event_state_color = "green"
                             elif event_state == 8:
                                 event_state_color = "yellow"
-                            csv_writer.writerow([intersectionName, intersectionID, moy, timestamp, signal_group, event_state_color])
+                            csv_writer.writerow([intersectionName, intersectionID, moy, timestamp, signal_group, event_state, event_state_color])
                     except:
                         print("Error extracting json info for line: " + str(textList[i]))
 
