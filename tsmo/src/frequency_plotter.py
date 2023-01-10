6c05ec3b8f1df368e807ff4d760d61516482e331
abey-yoseph <59704440+abey-yoseph@users.noreply.github.com>
2023-01-10 10:32:55 -0500

    Feature/uc3 frequency plots (#7)
    * initial checkin
    
    * remove old code
    
    * set number of x axis tick marks
    
    * add extra 5 seconds of data to plots for better visualization of run
    
    * initial checkin
    
    * Frequency plotting functionality added
    
    * Add stop bar location, only plot EV data
    
    * switch to using create timestamps, only plot streets tx
    
    * adding ET plotter back
    
    * create timestamp file for all message types, plot timestamp fix

---
+++ tsmo/src/frequency_plotter.py
@@ -0,0 +1,102 @@
+""" This is a plotting script that can be used to generate plots displaying the frequency of transmission
+or reception of a particular message. This script requires that timestamps have already been extracted from
+the appropriate kafka log, with the use of the "timestamp_parser.py" script."""
+
+## How to use this script:
+""" Run with python3 frequency_plotter.py messageType(MOM, SPAT) startTime(epoch milliseconds) endTime(epoch milliseconds)"""
+import pandas as pd
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+import math
+import constants
+import sys
+import os
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter, AutoMinorLocator)
+import matplotlib.dates as md
+import datetime as dt
+
+#This method will return an array containing the average tx/rx frequency of the message over a window of configurable size and
+#a datetime object for that frequency value.
+def averageCalc(df, window, messageType):
+    time_mean_arr = []
+    #iterate through the data in increments of "window" size
+    for i in range(0, len(df), window):
+        #Perform a diff on the timestamp column and store values in array
+        df["Diff(ms)"] = df['Timestamp(ms)'].diff()
+        windowVals = df["Diff(ms)"].iloc[i:i+window]
+
+        #convert to "frequency", need to check if there are "window" values to average first
+        if windowVals.mean() > 0 and len(windowVals) == window:
+            frequency = 1000 / float(windowVals.mean())
+            time_mean_arr.append([dt.datetime.fromtimestamp(df["Timestamp(ms)"].iloc[i]/1000), frequency])
+
+    return time_mean_arr
+
+#Plots the frequency values versus datetime for the specific message type using the start and end timestamps provided
+#by the user
+def plotter(messageType, start, end):
+    input_directory_path = f'{constants.DATA_DIR}/{constants.PARSED_OUTPUT_DIR}'
+    output_directory_path = f'{constants.DATA_DIR}/{constants.PLOT_DIR}'
+    
+    all_in_filenames = os.listdir(input_directory_path)
+    #find the parsed file with the message type of interest
+    for filename in all_in_filenames:
+        if messageType in filename and "timestamps" in filename:
+            data = pd.read_csv(f'{input_directory_path}/{filename}')
+    
+    #bin the data using the start and end time
+    start_end_data = data[(data['Timestamp(ms)'] >= int(start))&(data['Timestamp(ms)'] <= int(end))]
+    
+    min_time = dt.datetime.fromtimestamp(start_end_data['Timestamp(ms)'].iloc[0] / 1000)
+    max_time = dt.datetime.fromtimestamp(start_end_data['Timestamp(ms)'].iloc[-1] / 1000)
+
+    #Get the array of frequencies and datetimes for the message type, using a bin size of 10 messages
+    tenValueAverages = averageCalc(data, 10, messageType)           
+
+    #plot the message frequencies vs time, as well as the max/min range value based on message type
+    fig, ax1 = plt.subplots()
+    fig.set_size_inches(10, 10)        
+    plt.scatter([i[0] for i in tenValueAverages], [i[1] for i in tenValueAverages], c="blue", marker="^")
+    # spat broadcast requirement is 10Hz +/- 5, MOM is 1Hz +/- 0.5
+    if messageType == "spat":
+        plt.axhline(y=5, color='r', linestyle='--', label="frequency lower bound")
+        plt.axhline(y=15, color='r', linestyle='-', label="frequency upper bound")
+    elif messageType == "scheduling_plan":
+        plt.axhline(y=0.5, color='r', linestyle='--', label="frequency lower bound")
+        plt.axhline(y=1.5, color='r', linestyle='-', label="frequency upper bound")
+
+    plt.xticks(rotation=75)
+    axs=plt.gca()
+    xfmt = md.DateFormatter('%H:%M:%S') 
+    axs.xaxis.set_major_formatter(xfmt)
+    fig.autofmt_xdate()
+    plt.xlabel('Date-Time')
+    plt.ylabel('Frequency (Hz)')
+    plt.xlim(min_time, max_time)
+    fig.suptitle(filename.split("_")[4] + " " + filename.split("_")[5] + " " + filename.split("_")[6] + " " + str(messageType).replace("_", " ") + " message frequency")
+    plt.legend()
+    plotName = filename.split("_")[4] + "_" + filename.split("_")[5] + "_" + filename.split("_")[6] + "_" + str(messageType) + "_message_frequency.png"
+    plt.savefig(f'{output_directory_path}/{plotName}')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print('Run with: "python3 frequency_plotter.py messageType(MOM, SPAT) startTime(epoch milliseconds) endTime(epoch milliseconds)"')
+    else:  
+        message = sys.argv[1] 
+        start = sys.argv[2]
+        end = sys.argv[3]
+
+        #map user input to the file naming convention used in the kafka logs and then call the plotter method
+        messageType = ""
+        if message == "MOM":
+            messageType = "scheduling_plan"
+        elif message == "SPAT":
+            messageType = "spat"
+        else:
+            print("Please input a message type from the available options (MOM, SPAT)")
+            exit()
+
+        plotter(messageType, start, end)  
---
+++ tsmo/src/timestamp_parser.py
@@ -0,0 +1,54 @@
+""" This is a parsing script that can be used to extract timestamps from any of the kafka logfiles and write
+them to a csv file. """
+
+## How to use this script:
+""" Run with python3 timestamp_parser.py logName"""
+import sys
+from csv import writer
+import os
+import constants
+import json
+import pandas as pd
+import shutil
+import re
+
+#parser method to extract necessary fields from raw text file
+def kafkaParser(logname):
+    input_directory_path = f'{constants.DATA_DIR}/{constants.RAW_LOG_DIR}'
+    output_directory_path = f'{constants.DATA_DIR}/{constants.PARSED_OUTPUT_DIR}'
+    all_in_filenames = os.listdir(input_directory_path)
+
+    for file in all_in_filenames:
+        if logname in file:
+            fileName = file.split(".")[0]
+            #Convert the text file into an array of lines
+            with open(f'{input_directory_path}/{file}', encoding="utf8", errors='ignore') as textFile:
+                textList = []
+                for line in textFile:
+                    textList.append(line.strip().replace("\n", ""))
+
+            #write data of interest to csv which will be used to produce plots
+            with open(f'{output_directory_path}/{fileName}_timestamps.csv', 'w', newline='') as write_obj:
+                csv_writer = writer(write_obj)
+                csv_writer.writerow(["Message_Count", "Timestamp(ms)"])
+
+                count = 1
+                #Extract the CreateTime timestamp from the kafka data
+                for i in range(0, len(textList)):
+                    try:
+                        json_beg_index = textList[i].find("CreateTime")
+                        if (json_beg_index != -1):
+                            timestamp = re.sub("[^0-9]", "", textList[i].split(":")[1])                            
+                            csv_writer.writerow([count, timestamp])
+
+                            count += 1
+                    except:
+                        print("Error extracting json info for line: " + str(textList[i]))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Run with: "python3 timestamp_parser.py logname"')
+    else:       
+        logname = sys.argv[1]
+        kafkaParser(logname)
