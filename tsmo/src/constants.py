ea700342bc99b73fbe41c5de1048f049191b9f81
Abey <abey.yoseph@leidos.com>
2023-01-17 20:55:35 -0500

    split payload files by message type

--- tsmo/src/constants.py
+++ tsmo/src/constants.py
@@ -1,5 +1,11 @@
 DATA_DIR = '../data'
 RAW_LOG_DIR = 'Raw_Log_Files'
+RAW_PCAP_DIR = 'Raw_PCAP_Files'
+TSHARK_DIR = 'Tshark_Files'
+PAYLOAD_DIR = 'Payload_Files'
 INTERSECTION_MODEL_DIR = 'Intersection_Model'
 PARSED_OUTPUT_DIR = 'Parsed_Log_Output'
 PLOT_DIR = 'Plots'
+BSM_IDENTIFIER = "0014"
+MOM_IDENTIFIER = "00f3"
+MPM_IDENTIFIER = "00f2"
--- tsmo/src/platform_obu_pcap_parser.py
+++ tsmo/src/platform_obu_pcap_parser.py
@@ -26,8 +26,7 @@ def convert_pcap_to_csv(pcapFile):
                                                 --disable-protocol dvb-s2_gse -Tfields -Eseparator=, \
                                                     -e frame.time_epoch -e data.data > {tshark_directory_path}/tshark_{abs_filename}.csv')
 
-#creates csv with timestamp and only the J2735 payload
-def get_payload(row):
+def rowHelper(row):
     substrings_to_parse = [constants.BSM_IDENTIFIER, constants.MOM_IDENTIFIER, constants.MPM_IDENTIFIER]
 
     bsm_index = 10000
@@ -45,15 +44,33 @@ def get_payload(row):
     elif substrings_to_parse[2] in str(row[1]):
         mpm_index = row[1].find(substrings_to_parse[2])
 
-    if bsm_index < mom_index and bsm_index < mpm_index:
-        message = row[1][bsm_index:]
-    if mom_index < bsm_index and mom_index < mpm_index:
-        message = row[1][mom_index:]
-    if mpm_index < bsm_index and mpm_index < mom_index:
-        message = row[1][mpm_index:]
+    return [bsm_index, mom_index, mpm_index]
 
-    return message
+#creates csv with timestamp and only the J2735 payload
+def get_payload_bsm(row):
+    indices = rowHelper(row)
+    bsm = ""
+    if indices[0] < indices[1] and indices[0] < indices[2]:
+        bsm = row[1][indices[0]:]
 
+    return bsm
+
+def get_payload_mom(row):
+    indices = rowHelper(row)
+    mom = ""
+    if indices[1] < indices[0] and indices[1] < indices[2]:
+        mom = row[1][indices[1]:]
+
+    return mom
+
+def get_payload_mpm(row):
+    indices = rowHelper(row)
+    mpm = ""
+    if indices[2] < indices[1] and indices[2] < indices[0]:
+        mpm = row[1][indices[2]:]
+
+    return mpm
+    
 #gets the payload string from each row and decodes the desired fields
 def payloadHelper(pcapFile):
     tshark_directory_path = f'{constants.DATA_DIR}/{constants.TSHARK_DIR}'
@@ -66,11 +83,20 @@ def payloadHelper(pcapFile):
 
             #read the tshark file and create a file containing timestamps and payloads
             df_tshark_output = pd.read_csv(f'{tshark_directory_path}/{filename}')
-            df_data = {'timestamp': df_tshark_output.iloc[:,0], 'payload': df_tshark_output.apply(lambda row: get_payload(row), axis=1)}
+            df_data_bsm = {'Timestamp(ms)': (df_tshark_output.iloc[:,0]*1000), 'payload': df_tshark_output.apply(lambda row: get_payload_bsm(row), axis=1)}
+            df_data_mom = {'Timestamp(ms)': (df_tshark_output.iloc[:,0]*1000), 'payload': df_tshark_output.apply(lambda row: get_payload_mom(row), axis=1)}
+            df_data_mpm = {'Timestamp(ms)': (df_tshark_output.iloc[:,0]*1000), 'payload': df_tshark_output.apply(lambda row: get_payload_mpm(row), axis=1)}
 
-            payload = pd.DataFrame(data=df_data)
-            payload.dropna(subset=['payload'], inplace=True)
-            payload.to_csv(f'{payload_directory_path}/{abs_filename}_payload.csv', index=False)
+            payload_bsm = pd.DataFrame(data=df_data_bsm)
+            payload_mom = pd.DataFrame(data=df_data_mom)
+            payload_mpm = pd.DataFrame(data=df_data_mpm)
+
+            payload_bsm.dropna(subset=['payload'], inplace=True)
+            payload_mom.dropna(subset=['payload'], inplace=True)
+            payload_mpm.dropna(subset=['payload'], inplace=True)
+            payload_bsm.to_csv(f'{payload_directory_path}/{abs_filename}_payload_bsm.csv', index=False)
+            payload_mom.to_csv(f'{payload_directory_path}/{abs_filename}_payload_mom.csv', index=False)
+            payload_mpm.to_csv(f'{payload_directory_path}/{abs_filename}_payload_mpm.csv', index=False)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
