346f7d4f6c31b55a996eb39df4aad26dbad9636e
Adam Morrissett <22103567+adamlm@users.noreply.github.com>
2024-03-15 12:16:21 -0400

    Fix CARLA-CARMA time offset compensation and CSV headers (#33)
    * CDAR-849 Fix CARLA-CARMA time offset calculation
    
    This commit also updates the CSV header names and applies formatting.
    
    Related ticket: CDAR-850
    
    * CDAR-849 Replace custom pairwise function with more_itertools implementation
    
    Also remove unused imports.
    
    * CDAR-849 Fix iterator bug with clock messages
    
    * CDAR-849 Refactor closest timestamp search to be clearer
    
    Remove the indexing because it's not clear which indices are for which
    elements. Add clarifying comments for the format of bag_msgs.
    
    * CDAR-849 Add clarifying comments to closest timestamp function
    
    * CDAR-849 Relocate parse_ros_bags script
    
    The script is specific to CARMA Platform, so it does not belong in
    the carma-streets directory.
    
    * CDAR-849 Rearrange subtraction arguments for readability

---
+++ src/carma-platform/parse_ros_bags.py
@@ -0,0 +1,94 @@
+import argparse
+import csv
+from pathlib import Path
+
+import more_itertools
+import rosbag
+
+
+def find_msg_closest_to_timestamp(bag_msgs, timestamp):
+    for current, next_ in more_itertools.pairwise(bag_msgs):
+        # Each element in `bag_msgs` is a triple:
+        # <topic_name, msg, system_time_at_receive>
+        _, _, current_log_timestamp = current
+        _, _, next_log_timestamp = next_
+
+        # We only need to iterate until we find two messages who's logged
+        # timestamps straddle the timestamp of interest. All messages
+        # afterwards will have increasing differences.
+        if next_log_timestamp > timestamp:
+            if abs(current_log_timestamp - timestamp) < abs(timestamp - next_log_timestamp):
+                return current
+
+            return next_
+
+
+# calculate the offset between ros clock and the sim clock
+def get_time_offset(ros_bag_file):
+    with rosbag.Bag(ros_bag_file, "r") as bag:
+        cdasim_clock_msgs = bag.read_messages(topics=["/sim_clock"])  # CDASim clock
+        carla_clock_msgs = bag.read_messages(topics=["/clock"])  # CARLA clock
+
+        _, cdasim_clock_msg, cdasim_clock_log_time = next(cdasim_clock_msgs)
+
+        carla_clock_at_cdasim_start = find_msg_closest_to_timestamp(
+            carla_clock_msgs, cdasim_clock_log_time
+        )
+
+        return carla_clock_at_cdasim_start[1].clock - cdasim_clock_msg.clock
+
+
+def get_detected_objects(ros_bag_file, output_file, time_offset):
+    detected_obj_topic_name = "/environment/fused_external_objects"
+    messages = []
+    with rosbag.Bag(ros_bag_file, "r") as bag:
+        for _, msg, _ in bag.read_messages(topics=[detected_obj_topic_name]):
+            messages.extend(msg.objects)
+
+    output_file.parent.mkdir(exist_ok=True)
+
+    if output_file.exists():
+        print(f"Output file {output_file} already exists. Overwriting file.")
+
+    with open(output_file, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            ["cdasim_time_ms", "object_id", "map_position_x_m", "map_position_y_m"]
+        )
+        for message in messages:
+            cdasim_time_ms = (message.header.stamp - time_offset).to_sec() * 1_000
+            writer.writerow(
+                [
+                    cdasim_time_ms,
+                    message.id % 1_000,
+                    message.pose.pose.position.x,
+                    message.pose.pose.position.y,
+                ]
+            )
+
+    print(
+        f"Sorted data from topic '{detected_obj_topic_name}' saved to '{output_file}'"
+    )
+    print(f"Calculated sim time offset: '{time_offset}'")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Script to parse ROS Bags into CSV data"
+    )
+    parser.add_argument(
+        "--ros-bag-file", help="ROS Bags File.", type=str, required=True
+    )
+    parser.add_argument(
+        "--csv-dir", help="Directory to write csv file to.", type=Path, required=True
+    )
+    args = parser.parse_args()
+
+    time_offset = get_time_offset(args.ros_bag_file)
+    get_detected_objects(
+        args.ros_bag_file, args.csv_dir / "vehicle_detected_objects.csv", time_offset
+    )
+
+
+if __name__ == "__main__":
+    main()
