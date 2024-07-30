ebebafd28562ac0642d085d6ae68f364e02a6735
John Chrosniak <chrosniakj@gmail.com>
2024-07-30 15:46:38 -0400

    added script to check if message is published on specified topic

---
+++ src/carma-1-tenth/check_message_published.py
@@ -0,0 +1,27 @@
+# Check that a message exists in the bag
+
+import yaml
+import argparse, argcomplete
+import os
+
+
+def check_message_published(bag_dir, topic):
+    metadatafile : str = os.path.join(bag_dir, "metadata.yaml")
+    if not os.path.isfile(metadatafile):
+        raise ValueError("Metadata file %s does not exist. Are you sure %s is a valid rosbag?" % (metadatafile, bag_dir))
+    with open(metadatafile, "r") as f:
+        metadata_dict : dict = yaml.load(f, Loader=yaml.SafeLoader)["rosbag2_bagfile_information"]
+    topic_count_dict = {entry["topic_metadata"]["name"] : entry["message_count"] for entry in metadata_dict["topics_with_message_count"]}
+    if topic not in topic_count_dict:
+        return False
+    return topic_count_dict[topic] > 0
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description="Check that a message exists in the bag")
+    parser.add_argument("bag_in", type=str, help="Directory of bag to load")
+    parser.add_argument("topic", type=str, help="Topic to search for messages on")
+    argcomplete.autocomplete(parser)
+    args = parser.parse_args()
+    argdict : dict = vars(args)
+    result = check_message_published(os.path.normpath(os.path.abspath(argdict["bag_in"])), argdict["topic"])
+    print(result)
