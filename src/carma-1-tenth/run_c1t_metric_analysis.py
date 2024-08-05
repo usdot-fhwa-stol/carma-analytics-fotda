0cd2803c79f139021311f3558ed4b9411eee7771
John Chrosniak <chrosniakj@gmail.com>
2024-08-05 11:28:14 -0400

    more detailed names for testing metrics

--- src/carma-1-tenth/run_c1t_metric_analysis.py
+++ src/carma-1-tenth/run_c1t_metric_analysis.py
@@ -24,38 +24,38 @@ class C1TMetricAnalysis(unittest.TestCase):
         set_logger_level("rosbag2_storage", get_logging_severity_from_string("FATAL"))
         
 
-    def test_C1T_01(self):
+    def test_C1T_01_generates_route_from_MOM(self):
         # The C1T CMV is able to receive destinations as Mobility Operation messages from the infrastructure and generate a route
         self.assertTrue(check_message_published(self.bag_dir, "/plan"), "Message was not received on /plan")
 
-    def test_C1T_02(self):
+    def test_C1T_02_generates_route_from_rviz_timing(self):
         # The C1T CMV will generate a route within 3 seconds of receiving a goal pose from Rviz
         time_between_messages = check_message_timing(self.bag_dir, "/goal_pose", "/plan")
         self.assertTrue(np.all(time_between_messages < 3.0), "Time between messages on /goal_pose and /plan exceeded 3 seconds")
 
-    def test_C1T_03(self):
+    def test_C1T_03_generates_route_from_MOM_timing(self):
         # The C1T CMV will generate a route within 3 seconds of receiving an incoming mobility operation message
         time_between_messages = check_message_timing(self.bag_dir, "/incoming_mobility_operation", "/plan")
         self.assertTrue(np.all(time_between_messages < 3.0), "Time between messages on /incoming_mobility_operation and /plan exceeded 3 seconds")
 
-    def test_C1T_04(self):
+    def test_C1T_04_follow_route(self):
         # The C1T CMV will follow its generate routed to the destination with less than 0.4 m of crosstrack error 
         _, crosstrack_errors = plot_crosstrack_error(self.bag_dir, "/plan", show_plots=False)
         self.assertTrue(np.all(crosstrack_errors < 0.4), "Crosstrack error from planned route exceeded 0.4 m")
 
-    def test_C1T_05(self):
+    def test_C1T_05_follows_road_network(self):
         # The C1T CMV will drive along the centerline of the defined road network with less than 0.2 m of crosstrack error
         _, crosstrack_errors = plot_crosstrack_error(self.bag_dir, "/route_graph", show_plots=False)
         self.assertTrue(np.all(crosstrack_errors < 0.2), "Crosstrack error from road network exceeded 0.2 m")
 
-    def test_C1T_06(self):
-        # The C1T CMV achieves and mantains its target speed with a tolerance of 0.2 m/s (excluding turns)
+    def test_C1T_06_maintains_speed_on_straights(self):
+        # The C1T CMV achieves and maintains its target speed with a tolerance of 0.2 m/s (excluding turns)
         velocities, target_velocities = plot_vehicle_speed(self.bag_dir, show_plots=False)
         max_target_speed = np.max(target_velocities)
         speeds_on_straights = velocities[target_velocities == max_target_speed]
         self.assertTrue(np.all(np.abs(max_target_speed - speeds_on_straights) < 0.2), "Vehicle deviated more than 0.2 m/s from target speed on straight")
 
-    def test_C1T_07(self):
+    def test_C1T_07_slowdown_on_turns(self):
         # The C1T CMV will not slowdown less than 50% of the target speed during turns
         velocities, target_velocities = plot_vehicle_speed(self.bag_dir, show_plots=False)
         max_target_speed = np.max(target_velocities)
@@ -85,26 +85,26 @@ class C1TMetricAnalysis(unittest.TestCase):
         speeds_on_turns = velocities[relevant_velocity_idxs]
         self.assertTrue(np.all(speeds_on_turns > 0.5 * max_target_speed), "Vehicle speed reduced more than 50% on turn")
 
-    def test_C1T_08(self):
+    def test_C1T_08_stops_close_to_destination(self):
         # The C1T CMV stops within 0.2 m of the goal destinations
         arrival_distances = check_distance_to_arrival(self.bag_dir)
         self.assertTrue(np.all(arrival_distances < 0.2), "Vehicle did not stop within 0.2 m of goal destination")
 
-    def test_C1T_09(self):
+    def test_C1T_09_acks_with_MOM(self):
         # The C1T CMV sends Mobility Operation message acknowledging when a goal is reached
         self.assertTrue(check_message_published(self.bag_dir, "/outgoing_mobility_operation"), "Message not received on /outgoing_mobility_operation")
 
-    def test_C1T_10(self):
+    def test_C1T_10_infrastructure_response_to_ack_timing(self):
         # Infrastructure responds within a new goal destination within 1.5 seconds of receiving an ack from the C1T CMV
         time_between_messages = check_message_timing(self.bag_dir, "/outgoing_mobility_operation", "/incoming_mobility_operation")
         self.assertTrue(np.all(time_between_messages < 1.5), "Time between ack and new goal exceed 1.5 seconds")
 
-    def test_C1T_12(self):
+    def test_C1T_12_communicate_load_and_unload(self):
         # Infrastructure can communicate with the C1T CMV that the container is loaded and unloaded
         self.assertTrue(check_port_drayage_ack(self.bag_dir, "PICKUP"), "Vehicle did not ack PICKUP correctly")
         self.assertTrue(check_port_drayage_ack(self.bag_dir, "DROPOFF"), "Vehicle did not ack DROPOFF correctly")
 
-    def test_C1T_13(self):
+    def test_C1T_13_communicate_inspection(self):
         # Infrastructure can communicate with the C1T CMV that the container is inspected
         self.assertTrue(check_port_drayage_ack(self.bag_dir, "HOLDING_AREA"), "Vehicle did not ack HOLDING_AREA corectly")
     
