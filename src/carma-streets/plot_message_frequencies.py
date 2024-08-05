c4243fcee29c7e6c4f7cae3c7b863e292b5b1720
Misheel Bayartsengel <misheel.bayart@gmail.com>
2024-08-05 14:41:45 -0400

    508 complaint

--- src/carma-streets/plot_message_frequencies.py
+++ src/carma-streets/plot_message_frequencies.py
@@ -17,18 +17,18 @@ def plot_message_frequencies(csv_dir: Path, plots_dir: Path, simulation: bool = 
         simulation (bool,optional): bool flag to indicate whether data was collected in simulation. Defaults to False.
     """
     if csv_dir.is_dir():
-        if plots_dir.is_dir():
-            print(f"WARNING: {plots_dir} already exists. Contents will be overwritten.")
+        # if plots_dir.is_dir():
+        #     print(f"WARNING: {plots_dir} already exists. Contents will be overwritten.")
         plots_dir.mkdir(exist_ok=True)
         # Read CSV data
         message_data = dict([])
         time_sync_data = None
         for csv_file in csv_dir.glob("*.csv"):
-            print(f'Reading csv file {csv_file.name} ...')
+            # print(f'Reading csv file {csv_file.name} ...')
             if KafkaLogMessageType.TimeSync.value in csv_file.name:
                 time_sync_data = pd.read_csv(csv_file)
             elif KafkaLogMessageType.DetectedObject.value in csv_file.name:
-                # Since Detected Object messages are sent for each individual object they 
+                # Since Detected Object messages are sent for each individual object they
                 # do not have a static target frequency
                 continue
             else:
@@ -37,7 +37,7 @@ def plot_message_frequencies(csv_dir: Path, plots_dir: Path, simulation: bool = 
         fig, plots = plt.subplots(len(message_data), sharex=True, layout="constrained", figsize=[15,2.5*len(message_data)])
         # Add simulation time to message data
         for msg_plot,( message_name, message_data_frame) in zip( plots, message_data.items()):
-            print(f'Getting simulation time for {message_name} data ...')
+            # print(f'Getting simulation time for {message_name} data ...')
             if simulation:
                 message_data_frame["Time (ms)"] = get_simulation_time(message_data_frame["System Time (ms)"], time_sync_data["System Time (ms)"], time_sync_data["Message Time (ms)"])
                 message_data_frame["Time (s)"] = message_data_frame["Time (ms)"]/1000
@@ -51,13 +51,15 @@ def plot_message_frequencies(csv_dir: Path, plots_dir: Path, simulation: bool = 
                 # Any message with 10 Hz as target frequency uses window size 30 (3 s) for rolling average
                 add_message_frequency_columns(message_data_frame)
                 plot_message_frequency(msg_plot,message_data_frame['Time (s)'], message_data_frame["Average Frequency (Hz)"],message_name)
+            # print(min(message_data_frame["Average Frequency (Hz)"]), max(message_data_frame["Average Frequency (Hz)"]))
+        # print('\n')
         fig.suptitle('Message Frequency Plots', fontsize=20)
         fig.supxlabel('Time (s)', fontsize=16)
         fig.supylabel('Message Frequency (Hz)', fontsize=16)
         handles, labels = plots[-1].get_legend_handles_labels()
         fig.legend(handles, labels, loc='upper right', fontsize=16)
-        fig.savefig(f'{plots_dir}/message_frequencies.png')
-        
+        # fig.savefig(f'{plots_dir}/message_frequencies.png')
+
 def plot_message_frequency( axes: axes.Axes, time: list, frequency: list , message_name: str, target_frequency: float = 10.0, absolute_error: float = 2.0) :
     """Generate subplots for each message frequency
 
@@ -69,14 +71,20 @@ def plot_message_frequency( axes: axes.Axes, time: list, frequency: list , messa
         target_frequency (int, optional): Target frequency. Defaults to 10.
         absolute_error (int, optional): Acceptable deviation from target frequency. Defaults to 2.
     """
-    axes.scatter(time,frequency, c="blue", marker="^", label="Freq (hz)")
-    # Add horizontal lines for differing freq requirements based on message type
-    axes.axhline(y=target_frequency-absolute_error, color='r', linestyle='--', label="frequency lower bound")
-    axes.axhline(y=target_frequency+absolute_error, color='r', linestyle='-', label="frequency upper bound")
-    axes.set_title(message_name, fontsize=18)
-    axes.set_ylim(target_frequency-2*absolute_error, target_frequency+2*absolute_error)
-    axes.minorticks_on()
-    axes.grid(which='major', axis='both')
+    # print(message_name)
+    # if (message_name != 'sdsm'):
+    cleanedList = [x for x in frequency if str(x) != 'nan']
+    print(min(cleanedList), max(cleanedList))
+    # elif (message_name == 'sdsm'):
+    #     print('\n')
+    # axes.scatter(time,frequency, c="blue", marker="^", label="Freq (hz)")
+    # # Add horizontal lines for differing freq requirements based on message type
+    # axes.axhline(y=target_frequency-absolute_error, color='r', linestyle='--', label="frequency lower bound")
+    # axes.axhline(y=target_frequency+absolute_error, color='r', linestyle='-', label="frequency upper bound")
+    # axes.set_title(message_name, fontsize=18)
+    # axes.set_ylim(target_frequency-2*absolute_error, target_frequency+2*absolute_error)
+    # axes.minorticks_on()
+    # axes.grid(which='major', axis='both')
 
 def get_simulation_time(message_wall_time : list, time_sync_wall_time: list, time_sync_simulation_time : list)-> list:
     """Returns a list of simulation times for the provided message wall times.
@@ -115,20 +123,21 @@ def add_message_frequency_columns( messages: pd.DataFrame, window: int = 30) -> 
     messages['Interval (s)'] = messages['Time (s)'].diff()
     messages['Average Interval (s)'] = messages['Interval (s)'].rolling(window=window).mean()
     messages['Average Frequency (Hz)'] = (1/messages['Average Interval (s)'])
-    print(messages["Average Interval (s)"].describe())
-    print(messages["Average Frequency (Hz)"].describe())
+    # print(messages["Average Interval (s)"].describe())
+    # print(messages["Average Frequency (Hz)"].describe())
     return messages
 
 
 
 def main():
     parser = argparse.ArgumentParser(description='Script to plot message frequency from CARMA Streets message csv data.')
-    parser.add_argument('--csv-dir', help='Directory to read csv data from.', type=Path, required=True)  
-    parser.add_argument('--plots-dir', help='Directory to save generated plots.', type=Path, required=True) 
+    parser.add_argument('--csv-dir', help='Directory to read csv data from.', type=Path, required=True)
+    parser.add_argument('--plots-dir', help='Directory to save generated plots.', type=Path, required=True)
     parser.add_argument('--simulation', help='Flag indicating data is from simulation', action='store_true', default=False)
 
     args = parser.parse_args()
     plot_message_frequencies(args.csv_dir, args.plots_dir, args.simulation)
+    print("\n")
 
 if __name__ == '__main__':
     main()
