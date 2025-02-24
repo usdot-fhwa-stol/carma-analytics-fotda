ca0e584975353e52d673fbd018b801a467b48768
Misheel Bayartsengel <misheel.bayart@gmail.com>
2025-02-24 06:27:03 -0500

    Fix test

--- src/carma-platform/test/test_guidance_scripts.py
+++ src/carma-platform/test/test_guidance_scripts.py
@@ -461,7 +461,8 @@ def test_run_steering_wheel_analysis(mock_mcap_path):
         mock_extract.return_value = {
             "/hardware_interface/as/pacmod/parsed_tx/steer_rpt": (
                 timestamps,
-                (actual_values, cmd_values)
+                actual_values,
+                cmd_values
             ),
         }
 
@@ -501,7 +502,8 @@ def test_run_steering_wheel_analysis_fails_threshold(mock_mcap_path):
         mock_extract.return_value = {
             "/hardware_interface/as/pacmod/parsed_tx/steer_rpt": (
                 timestamps,
-                (actual_values, cmd_values)
+                actual_values,
+                cmd_values
             )
         }
 
