58ee64d83d229593c3ba66c75ef0375854230494
Anish <Anishdeva17@gmail.com>
2025-03-31 10:51:57 -0400

    add speed analysis

---
+++ src/carma-platform/run_all_regression_analysis.py
@@ -0,0 +1,247 @@
+from pathlib import Path
+from typing import Dict
+from guidance_scripts import (
+    get_engage_time,
+    run_crosstrack_analysis,
+    run_turn_accuracy_analysis,
+    run_acceleration_comfort_analysis,
+    run_lateral_analysis,
+    run_guidance_steering_analysis,
+    run_steering_wheel_analysis,
+    get_planner_trajectory_intervals,
+    run_guidance_speed_analysis
+)
+from run_all_analysis import run_all_analysis
+import argparse
+import argcomplete
+from environment_scripts import (
+    extract_lanelet2_map_from_mcap,
+    filter_map_points_for_trajectory,
+    plot_2d_map_and_pose,
+)
+
+# VARIOUS THRESHOLDS FOR THE METRICS
+# 1. Cross_track analysis
+CROSS_TRACK_ERROR_THRESHOLD_METER = 2.0
+# 2. Turn accuracy analysis
+TURN_ACCURACY_ERROR_THRESHOLD_METER = 2.0
+# 3. Acceleration comfort analysis
+COMFORT_ACCELERATION_THRESHOLD_MS2 = 3.0
+# 4. Lateral acceleration jerk analysis
+ACC_THRESHOLD_TO_PASS_MS2 = 2.0
+JERK_THRESHOLD_TO_PASS_MS3 = 3.0
+# 5. Steering angle and steering wheel analysis
+STEERING_ANGLE_ERROR_THREHOLD_RADIAN = 0.1
+STEERING_WHEEL_ANGLE_ERROR_THRESHOLD_RADIAN = 0.1
+# 6. Speed analysis
+SPEED_THRESHOLD_TO_PASS_MPH = 2.0
+
+
+def analyze_mcap_file_for_regression_analysis(
+    mcap_path: Path, output_dir: Path, stats_dir: Path, data_dir: Path, plots_dir: Path
+) -> list:
+    """Extract single MCAP file and run all control analysis on it"""
+    # 0. General steps needed for all
+    try:
+        engage_time, disengage_time = get_engage_time(mcap_path)
+    except Exception as e:
+        print(f"Error getting engage time for mcap {mcap_path}: {e}")
+        return None
+
+    # Run all the tests for engage to disengage
+    # and specifically for lane change durations:
+    intervals = [(engage_time, disengage_time)]
+    # planner_plugin_name = "/guidance/plugins/cooperative_lanechange"
+    # try:
+    #     new_intervals = get_planner_trajectory_intervals(
+    #         mcap_path=mcap_path,
+    #         planner_plugin_name=planner_plugin_name,
+    #         start_time=engage_time,
+    #         end_time=disengage_time,
+    #     )
+    #     intervals.extend(new_intervals)
+    # except Exception as e:
+    #     print(
+    #         f"Error getting start and end time {planner_plugin_name}: for mcap {mcap_path}: {e}"
+    #     )
+    #     return None
+
+    all_analysis_stats = []
+    for start_time, end_time in intervals:
+        analysis_stats = {}
+        # # 1. Cross_track analysis
+        # try:
+        #     is_passed, _, _, _, _ = run_crosstrack_analysis(
+        #         mcap_path,
+        #         CROSS_TRACK_ERROR_THRESHOLD_METER,
+        #         start_time,
+        #         end_time,
+        #         stats_dir,
+        #         data_dir,
+        #         plots_dir,
+        #     )
+        #     analysis_stats["run_crosstrack_analysis"] = is_passed
+        # except Exception as e:
+        #     print(
+        #         f"Error analyzing {mcap_path} for metric run_crosstrack_analysis: {e}"
+        #     )
+        #     analysis_stats["run_crosstrack_analysis"] = None
+
+        # # 2. Turn accuracy analysis by spline fitting
+        # try:
+        #     is_passed, _, _, actual_path, planned_path, _, _ = (
+        #         run_turn_accuracy_analysis(
+        #             mcap_path,
+        #             TURN_ACCURACY_ERROR_THRESHOLD_METER,
+        #             start_time,
+        #             end_time,
+        #             stats_dir,
+        #             data_dir,
+        #             plots_dir,
+        #         )
+        #     )
+        #     analysis_stats["run_turn_accuracy_analysis"] = is_passed
+        # except Exception as e:
+        #     print(
+        #         f"Error analyzing {mcap_path} for metric run_turn_accuracy_analysis: {e}"
+        #     )
+        #     analysis_stats["run_turn_accuracy_analysis"] = None
+
+        # # 2.a Visualize traveled path and planned path on actual map
+        # # This is for visual verification
+        # try:
+        #     lanelet2_map_data = extract_lanelet2_map_from_mcap(str(mcap_path))
+        #     if list(planned_path):
+        #         plot_2d_map_and_pose(
+        #             lanelet2_map_data, list(planned_path), plots_dir / "planned_path"
+        #         )
+        #     if list(actual_path):
+        #         plot_2d_map_and_pose(
+        #             lanelet2_map_data, list(actual_path), plots_dir / "actual_path"
+        #         )
+        # except Exception as e:
+        #     print(f"Error plotting {mcap_path} for plotting lanelet2 map data: {e}")
+
+        # # 3. Longitudinal acceleration analysis
+        # try:
+        #     is_passed, _, _, _, _, _, _, _ = run_acceleration_comfort_analysis(
+        #         mcap_path,
+        #         COMFORT_ACCELERATION_THRESHOLD_MS2,
+        #         start_time,
+        #         end_time,
+        #         stats_dir,
+        #         data_dir,
+        #         plots_dir,
+        #     )
+        #     analysis_stats["run_acceleration_comfort_analysis"] = is_passed
+        # except Exception as e:
+        #     print(
+        #         f"Error analyzing {mcap_path} for metric run_acceleration_comfort_analysis: {e}"
+        #     )
+        #     analysis_stats["run_acceleration_comfort_analysis"] = None
+
+        # # 4. Lateral acceleration and jerk analysis for instant and any 1sec window period
+        # try:
+        #     is_passed, _, _, _, _, _, _, _, _, _, _ = run_lateral_analysis(
+        #         mcap_path,
+        #         ACC_THRESHOLD_TO_PASS_MS2,
+        #         JERK_THRESHOLD_TO_PASS_MS3,
+        #         start_time,
+        #         end_time,
+        #         stats_dir,
+        #         data_dir,
+        #         plots_dir,
+        #     )
+        #     analysis_stats["run_lateral_analysis"] = is_passed
+        # except Exception as e:
+        #     print(f"Error analyzing {mcap_path} for metric run_lateral_analysis: {e}")
+        #     analysis_stats["run_lateral_analysis"] = None
+
+        # # 5.A Steering angle analysis
+        # try:
+        #     steering_analysis_results = run_guidance_steering_analysis(
+        #         mcap_path,
+        #         STEERING_ANGLE_ERROR_THREHOLD_RADIAN,
+        #         start_time,
+        #         end_time,
+        #         stats_dir,
+        #         data_dir,
+        #         plots_dir,
+        #     )
+        #     analysis_stats["run_guidance_steering_analysis"] = (
+        #         steering_analysis_results[0]
+        #     )
+
+        # # 5.B Steering wheel angle analysis
+        # try:
+        #     steering_wheel_analysis_results = run_steering_wheel_analysis(
+        #         mcap_path,
+        #         STEERING_ANGLE_ERROR_THREHOLD_RADIAN,
+        #         start_time,
+        #         end_time,
+        #         stats_dir,
+        #         data_dir,
+        #         plots_dir,
+        #     )
+        #     analysis_stats["run_steering_wheel_analysis"] = (
+        #         steering_wheel_analysis_results[0]
+        #     )
+
+        # except Exception as e:
+        #     print(
+        #         f"Error analyzing {mcap_path} for metric run_guidance_speed_analysis: {e}"
+        #     )
+        #     analysis_stats["run_guidance_speed_analysis"] = None
+        # except Exception as e:
+        #     print(
+        #         f"Error analyzing {mcap_path} for metric run_speed_analysis: {e}"
+        #     )
+        #     analysis_stats["run_speed_analysis"] = None
+        # 6.A Speed analysis
+        try:
+            speed_analysis_results = run_guidance_speed_analysis(
+                mcap_path,
+                SPEED_THRESHOLD_TO_PASS_MPH,
+                start_time,
+                end_time,
+                stats_dir,
+                data_dir,
+                plots_dir,
+            )
+        except Exception as e:
+            print(
+                f"Error analyzing {mcap_path} for metric run_guidance_speed_analysis: {e}"
+            )
+            analysis_stats["run_guidance_speed_analysis"] = None
+
+        all_analysis_stats.append(analysis_stats)
+
+    return all_analysis_stats
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Run all control analysis on multiple MCAP files in a given directory"
+    )
+    parser.add_argument(
+        "--input-dir",
+        type=Path,
+        help="Directory containing MCAP files to analyze",
+        required=True,
+    )
+    parser.add_argument(
+        "--output-dir",
+        type=Path,
+        help="Base directory for saving analysis results (optional)",
+        default=None,
+    )
+    argcomplete.autocomplete(parser)
+    args = parser.parse_args()
+
+    try:
+        run_all_analysis(
+            args.input_dir, analyze_mcap_file_for_regression_analysis, args.output_dir
+        )
+    except Exception as e:
+        print(f"Error: {e}")
+        exit(1)
